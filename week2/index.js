7d047c5ce53559fc169d1458dee57842400cae91
Michael Eisenbraun <eisenbm@algonquincollege.com>
2018-05-22 15:09:21 -0400

    Updates files for week 2

--- week2/index.js
+++ week2/index.js
@@ -24,3 +24,13 @@ favoriteFood.push('greens')
 console.log(favoriteFood)
 favoriteFood[2] = 'Pizza'
 console.log(favoriteFood)
+
+const threeLittlePigs = {
+  number: 5,
+  adjective: 'cute',
+  noun: 'dog',
+  verbs: ['run', 'dance', 'jump', 'eat', 'drink', 'lie'],
+  pluralNouns: ['students', 'geese', 'boys']
+}
+
+console.log(`Once upon a time a time, there were ${threeLittlePigs.number} ${threeLittlePigs['adjective']} pigs. One day, their ${threeLittlePigs.noun} said, "You are all grown up and must ${threeLittlePigs.verbs[0]} on your own." So they left to ${threeLittlePigs.verbs[1]} their houses.The first little pig wanted only to ${threeLittlePigs.verbs[2]} all day and quickly built his house out of ${threeLittlePigs.pluralNouns[0]}.The second little pig wanted to ${threeLittlePigs.verbs[3]} and ${threeLittlePigs.verbs[4]} all day so he built his house with ${threeLittlePigs.pluralNouns[1]}.The third little pig knew the wolf lived nearby and worked hard to ${threeLittlePigs.verbs[5]} his house out of ${threeLittlePigs.pluralNouns[2]}.`)
