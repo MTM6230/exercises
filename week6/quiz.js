8122a5c96a1672435fac70126d3cc72246c1ebcf
Michael Eisenbraun <eisenbm@algonquincollege.com>
2018-06-21 14:17:00 -0400

    Adds JavaScript for Quiz for Week 9.

---
+++ week6/quiz.js
@@ -0,0 +1,16 @@
+const character = 'Hercules'
+
+// 1
+console.log(character.toUpperCase())
+
+// 2
+console.log(character.split(''))
+
+// 3
+console.log(character.split('').reverse())
+
+// 4
+console.log(character.split('').reverse().join(''))
+
+// 5
+console.log(character.includes('Hero'))
