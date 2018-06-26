30dcd21bc7c4efb29cb9c53b0512baf8de4a68af
Michael Eisenbraun <eisenbm@algonquincollege.com>
2018-06-26 11:56:03 -0400

    Adds the additional code for Coding Challenge for class 8.2

--- week8/dice.js
+++ week8/dice.js
@@ -1,1 +1,23 @@
+const colors = ['red', 'blue', 'yellow', 'green', 'orange', 'purple']
 
+const die1 = document.getElementById('die1')
+const side = die1.querySelector('.side')
+
+function randomInt(max) {
+  return Math.floor(Math.random() * max)
+}
+
+const random = randomInt(colors.length)
+const color = colors[random]
+die1.classList.add(color)
+
+side.textContent = random + 1
+
+const die2 = document.getElementById('die2')
+const sides = die2.getElementsByClassName('side')
+
+for (const side of sides) {
+  side.classList.add('hide')
+}
+
+sides[randomInt(sides.length)].classList.remove('hide')
